1b5f3861c29d23714c72dd2bb600cc1bcfecaf67
stellvia2359 <stellvia2359@gmail.com>
2021-04-08 01:59:27 +0800

    template

--- content/js/site.js
+++ content/js/site.js
@@ -9,7 +9,7 @@ var site = {
         if (urlParams.has('page')) {
             var url = urlParams.get('page');
             var loadeqjson = urlParams.get('loadeqjson');
-            site.loadPage(url,loadeqjson);
+            site.loadPage(url, loadeqjson);
         } else {
             site.loadHome();
         }
@@ -34,7 +34,7 @@ var site = {
         });
         utility.loadTooltip();
     },
-    loadPage: function (url,loadeqjson) {
+    loadPage: function (url, loadeqjson) {
         $.ajax({
             url: url + ".html",
             dataType: "html",
@@ -43,14 +43,15 @@ var site = {
                 $(".nav-link").removeClass("active");
                 var navId = $(".nav-id").val();
                 $("#" + navId).addClass("active");
-                if(loadeqjson){
-                    site.loadJson(url);
+                if (loadeqjson) {
+                    var json = site.loadEquipmentDataOfAllCategories(url);
+                    site.loadTemplate(json);
+
+
                 }
-                else{
-                    utility.loadPopover();
-                    utility.generateStars();
-                    utility.hideLoadingMask();
-                }
+                utility.loadPopover();
+                utility.generateStars();
+                utility.hideLoadingMask();
             }
         });
     },
@@ -67,27 +68,94 @@ var site = {
             }
         });
     },
-    loadJson: function (url) {
+    loadCommentDB: function (url) {
+        var comments;
         $.ajax({
             url: url + ".json",
             dataType: "json",
+            async: false,
             success: function (data) {
-                $.each(data.allEquipments, function (index, item) {
-                    item.equipments.sort(utility.GetSortOrder("star"));
-                    item.cards.sort(utility.GetSortOrder("star"));
-                });
-                data.costumeEnchantments.sort(utility.GetSortOrder("star"));
-                var template;
-                $.get("guide/common/template_equipment.txt", function (value) {
-                    template = $.templates(value);
-                    var htmlOutput = template.render(data);
-                    $("#equipments-block").html(htmlOutput);
-                    utility.loadPopover();
-                    utility.generateStars();
-                    utility.hideLoadingMask();
-                });
+                comments = data;
             }
         });
+        return comments;
+    },
+    loadEquipmentDB: function () {
+        var result;
+        $.ajax({
+            url: "guide/common/equipments.json",
+            dataType: "json",
+            async: false,
+            success: function (data) {
+                result = data;
+            }
+        });
+        return result;
+    },
+    loadEquipmentDataOfAllCategories: function (url) {
+        var commentDB = site.loadCommentDB(url);
+        site.printJson(commentDB);
+        
+        var equipmentDB = site.loadEquipmentDB();
+        site.printJson2(equipmentDB);
+
+        var finalCategories = [];
+        $.each(commentDB.equipments, function (i, category) {
+
+            var targetCategory = equipmentDB.equipments.filter(function (c) {
+                return c.categoryCode == category.categoryCode
+            })[0];
+            var finalCategory = JSON.parse(JSON.stringify(targetCategory));
+            finalCategory.items = [];
+            finalCategory.cards = [];
+            $.each(category.items, function (j, item) {
+                var targetItem = targetCategory.items.filter(function (e) {
+                    return e.name == item.name
+                })[0];
+                targetItem.star = item.star;
+                targetItem.comments = item.comments;
+                finalCategory.items.push(targetItem);
+            });
+            $.each(category.cards, function (j, card) {
+                var targetCard = targetCategory.cards.filter(function (c) {
+                    return c.name == card.name
+                })[0];
+                targetCard.star = card.star;
+                targetCard.comments = card.comments;
+                finalCategory.cards.push(targetCard);
+            });
+            finalCategories.push(finalCategory);
+        });
+
+        result = {
+            equipments: finalCategories
+        }
+        return result;
+    },
+    loadTemplate: function (json) {
+        var template;
+        $.ajax({
+            url: "guide/common/template_equipment.txt",
+            async: false,
+            success: function (data) {
+                template = $.templates(data);
+                var htmlOutput = template.render(json);
+                $("#equipments-block").html(htmlOutput);
+
+            }
+        });
+    },
+    printJson: function (data) {
+        var x = {
+            equipments: data.equipments.map(e => ({ categoryCode: e.categoryCode, items: e.items.map(i => ({ name: i.name, star: i.star, comments: i.comments })), cards: e.cards.map(c => ({ name: c.name, star: c.star, comments: c.comments })) }))
+        };
+        console.log(x);
+    },
+    printJson2: function (data) {
+        var x = {
+            equipments: data.equipments.map(e => ({ category:e.category,categoryCode: e.categoryCode, overrideCategoryName: e.overrideCategoryName, hasCard: e.hasCard, items: e.items.map(i => ({ imgNames: i.imgNames, name: i.name, slots: i.slots, available: i.available, loot: i.loot, externalUrls: i.externalUrls, infos: i.infos })), cards: e.cards.map(c => ({ imgNames: c.imgNames, name: c.name, available: c.available, infos: c.infos })) }))
+        };
+        console.log(x);
     }
 }
 
